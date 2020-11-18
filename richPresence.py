588289a4fa015bb5e9eb5c7c2ea3e16482a294d1
kjcdev <kjciastek@gmail.com>
2020-11-18 08:12:31 +0100

    Created socket instance, added if statements to recognize type of session, created getPlayerIndex function

--- richPresence.py
+++ richPresence.py
@@ -1,8 +1,43 @@
+from f1_2020_telemetry.packets import *
+import socket
 from pypresence import Presence
 
 client_id = ''
 RPC = Presence(client_id, pipe=0)
 RPC.connect()
 
+udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+udp_socket.settimeout(1.0)
+udp_socket.bind(('', ))
+
+while True:
+    try:
+        udp_packet = udp_socket.recv(2048)
+    except socket.timeout:
+        continue
+
+    packet = unpack_udp_packet(udp_packet)
+
+    if (isinstance(packet, PacketSessionData_V1)):
+        # race
+        if(packet.sessionType==10):
+        # q1
+        elif(packet.sessionType==5):
+        # q2
+        elif(packet.sessionType==6):
+        # q3
+        elif(packet.sessionType==7):
+        # short quali
+        elif(packet.sessionType==8):
+        # one shot quali
+        elif(packet.sessionType==9):
+        # time trial
+        elif(packet.sessionType==12):
+
+def getPlayerIndex():
+    udp_packet = udp_socket.recv(2048)
+    packet = unpack_udp_packet(udp_packet)
+    if(isinstance(packet, PacketHeader)):
+        return packet.playerCarIndex
 
 
